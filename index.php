4e761df4d1f47d597552848c838bbdd3e1a48329
leilycoquard <leilycoquard@gmail.com>
2022-12-13 14:05:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Plateforme d’organisation d’événements écoresponsables - Tcheen</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+
+
+        <?php
+            require_once 'header.php';
+            require_once 'section1.php';
+            require_once 'section2.php';
+            require_once 'section3_economisez.php';
+            require_once 'section4.php';
+            require_once 'section5.php';
+            require_once 'section6.php';
+            require_once 'section7.php';
+            require_once 'section8.php';
+            require_once 'footer.php';
+        ?>
+
+    </body>
+</html>
