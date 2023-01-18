27fd81daf181914ae7ff8bda13f22945f5da3a42
leilycoquard <leilycoquard@gmail.com>
2023-01-18 11:14:14 +0100

    liens support

--- section10_soutien.php
+++ section10_soutien.php
@@ -7,15 +7,15 @@
         </div>
     
         <div>
-            <a href="https://technowest.com/" target="_blank"></a><img src="webp\support\technow.webp" alt="Bordeaux Technowest" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.nouvelle-aquitaine.fr/" target="_blank"></a><img src="webp\support\naq.webp" alt="Région Nouvelle Aquitaine" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.frenchtechbordeaux.com/" target="_blank"></a><img src="webp\support\ftech.webp" alt="French Tech Bordeaux" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.bpifrance.fr/" target="_blank"></a><img src="webp\support\bpi.webp" alt="Bpifrance" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://la-ruche.net/" target="_blank"></a><img src="webp\clients\laruche.webp" alt="La Ruche" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://makesense.org/" target="_blank"></a><img src="webp\support\msense.webp" alt="Make Sense" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.reseau-entreprendre.org/fr/accueil/" target="_blank"></a><img src="webp\support\rentreprendre.webp" alt="Réseau entreprendre" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.lanef.com/" target="_blank"></a><img src="webp\support\nef.webp" alt="La Nef" width="auto" height="100%" loading="lazy"></a>
-            <a href="https://www.tourismelab.fr/" target="_blank"></a><img src="webp\support\tourisme.webp" alt="Tourisme Lab" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://technowest.com/" target="_blank"><img src="webp\support\technow.webp" alt="Bordeaux Technowest" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.nouvelle-aquitaine.fr/" target="_blank"><img src="webp\support\naq.webp" alt="Région Nouvelle Aquitaine" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.frenchtechbordeaux.com/" target="_blank"><img src="webp\support\ftech.webp" alt="French Tech Bordeaux" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.bpifrance.fr/" target="_blank"><img src="webp\support\bpi.webp" alt="Bpifrance" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://la-ruche.net/" target="_blank"><img src="webp\clients\laruche.webp" alt="La Ruche" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://makesense.org/" target="_blank"><img src="webp\support\msense.webp" alt="Make Sense" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.reseau-entreprendre.org/fr/accueil/" target="_blank"><img src="webp\support\rentreprendre.webp" alt="Réseau entreprendre" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.lanef.com/" target="_blank"><img src="webp\support\nef.webp" alt="La Nef" width="auto" height="100%" loading="lazy"></a>
+            <a href="https://www.tourismelab.fr/" target="_blank"><img src="webp\support\tourisme.webp" alt="Tourisme Lab" width="auto" height="100%" loading="lazy"></a>
         </div>
        
     </div>
